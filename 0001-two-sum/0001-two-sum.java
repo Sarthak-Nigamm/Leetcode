948a7f373aac8597bf85cc52dec485b8f8c4755d
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-07-06 21:34:14 +0530

    Time: 45 ms (25.31%), Space: 47.2 MB (23.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // i chalega second-last element tak
-        for (int i = 0; i < nums.length - 1; i++) {
-            // j hamesha i ke aage se shuru hoga
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j}; // Direct array return kar do
-                }
+        int i =0;
+     while(i<nums.length-1){
+        for(int j =i+1; j<nums.length; j++){
+            if(nums[i]+nums[j] == target){
+                return new int[] {i,j};
             }
         }
-        return new int[] {}; // Agar koi pair na mile 
-    }
+        i++;
+     }
+  return new int[]{};  }
 }
