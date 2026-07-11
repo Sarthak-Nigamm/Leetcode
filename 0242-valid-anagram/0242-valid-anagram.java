c374d25e4dd7542d7c5210b8a319b12c7aafedb0
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-07-11 14:37:28 +0530

    Time: 6 ms (43.52%), Space: 44.4 MB (66.98%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int arr1[] = new int[256];
+        int arr2[] = new int[256];
+        for(int i =0; i<s.length(); i++){
+            int num = s.charAt(i);
+            arr1[num] ++;
+            int num2 = t.charAt(i); 
+            arr2[num2]++ ;
+        }
+
+        for(int i =0; i<256; i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+       return true; }
+}
