3e90f8f51ee8da60c5e1316060c9fc260eb9edda
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-07-12 01:05:50 +0530

    Time: 0 ms (100%), Space: 48.3 MB (68.98%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int si =0;
+        int ei = nums.length-1;
+        while(si<=ei){
+            int mid = si+(ei-si)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                ei = mid-1;
+            }else{
+                si = mid+1;
+            }
+        }
+ return -1;   }
+}
