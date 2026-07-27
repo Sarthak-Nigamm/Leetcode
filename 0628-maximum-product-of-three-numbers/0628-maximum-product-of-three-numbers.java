f60b42f0a734092d223e3d9d604f4d99d2aeab97
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-07-27 05:34:47 +0530

    Time: 2 ms (99.61%), Space: 47.2 MB (93.88%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,13 +1,32 @@
-import java.util.*;
 class Solution {
-    public int maximumProduct(int[] arr) {
-      Arrays.sort(arr);
-        int n = arr.length-1;
-        if(arr[0]*arr[1]*arr[n] > arr[n]*arr[n-1]*arr[n-2]){
-            return arr[0]*arr[1]*arr[n] ;
+    public int maximumProduct(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+
+        for (int n : nums) {
+            // Sabse bade 3 numbers update karna
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            // Sabse chhote 2 numbers update karna
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
         }
-        else if(arr[0]*arr[1]*arr[n] <= arr[n]*arr[n-1]*arr[n-2]){
-             return arr[n]*arr[n-1]*arr[n-2] ;
-        }
-   return -1; }
+
+        // Ya toh teeno sabse bade positive numbers ka product
+        // Ya fir do sabse chhote negative numbers aur ek sabse bade positive number ka product
+        return Math.max(max1 * max2 * max3, min1 * min2 * max1);
+    }
 }
