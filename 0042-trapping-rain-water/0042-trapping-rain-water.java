6112faa62ddc1875c72ccab7278d60e5e6efa5f7
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-08-16 19:22:37 +0530

    Time: 1 ms (58.4%), Space: 49 MB (9.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,22 @@
 class Solution {
-    public int trap(int[] height) {
-        int n = height.length;
-        //leftmax
-        int leftmax[] = new int[n];
-        leftmax[0] = height[0];
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int[]left = new int[n];
+        left[0] = arr[0];
         for(int i =1; i<n; i++){
-            leftmax[i] = Math.max(leftmax[i-1], height[i]);
-        }
-        //rightmax
-        int rightmax[] =new int[n];
-        rightmax[n-1] = height[n-1];
-        for(int i = n-2; i>=0; i--){
-            rightmax[i] = Math.max(rightmax[i+1], height[i]);
-        } 
-        int twl =0;
-        for(int i=0; i<n; i++){
-        int curwl = (int) Math.min(leftmax[i],rightmax[i]) - height[i];
-        twl+=curwl;
+            left[i] = Math.max(left[i-1], arr[i]);
         }
 
-   return twl; }
+        int [] right = new int[n];
+        right[n-1] = arr[n-1];
+        for(int i =n-2; i>=0; i--){
+            right[i] = Math.max(right[i+1],arr[i]);
+        }
+
+        int twl = 0;
+        for(int i =0; i<n; i++){
+            int wl = Math.min(left[i],right[i]) - arr[i] ;
+            twl +=wl;
+        }
+  return twl;  }
 }
