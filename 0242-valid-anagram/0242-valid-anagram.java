8a14aff3d2ba46feee3de244af780c8225343a37
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-08-24 17:42:58 +0530

    Time: 6 ms (43.44%), Space: 44.3 MB (82.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,16 +3,16 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int arr1[] = new int[256];
-        int arr2[] = new int[256];
+        int arr1[] = new int[26];
+        int arr2[] = new int[26];
         for(int i =0; i<s.length(); i++){
-            int num = s.charAt(i);
+            int num = s.charAt(i)-'a';
             arr1[num] ++;
-            int num2 = t.charAt(i); 
+            int num2 = t.charAt(i)-'a'; 
             arr2[num2]++ ;
         }
 
-        for(int i =0; i<256; i++){
+        for(int i =0; i<26; i++){
             if(arr1[i]!=arr2[i]){
                 return false;
             }
