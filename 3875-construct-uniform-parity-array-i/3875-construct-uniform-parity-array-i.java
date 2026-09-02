025e053fae94ad75c3632b847fdeb02d085eed73
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-02 18:47:27 +0530

    Time: 1 ms (31.89%), Space: 44.9 MB (87.6%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,4 +1,25 @@
 class Solution {
+
     public boolean uniformArray(int[] nums1) {
-      return true; }
+
+        boolean op = false;
+
+        boolean ae = false;
+
+        for(int i =0; i<nums1.length; i++){
+
+            if(nums1[i]%2!=0){
+
+                op = true;
+
+                return op;
+
+            }
+
+        }
+
+
+
+  return op == false;  }
+
 }
