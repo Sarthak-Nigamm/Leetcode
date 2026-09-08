923dd8f25f0309a54768f16f431e85da810acbac
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-08 19:10:59 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,9 +3,5 @@ class Solution {
         if(n<1000){
             return 0;
         }
-        int count = 0;
-        for(int i =1000; i<=n; i++){
-            count++;;
-        }
-      return count; }
+       return (n-1000+1);}
 }
