3b10a2c48630519cb52a72c9da1db5048eb05536
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-08 19:09:40 +0530

    Time: 4 ms (16.64%), Space: 42.5 MB (57.08%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,20 +1,11 @@
 class Solution {
-    public int count(int n){
-        int count = 0;
-        while(n%1000!=n){
-            count++;
-            n/=1000;
-        }
-      return count; 
-    }
     public int countCommas(int n) {
         if(n<1000){
             return 0;
         }
         int count = 0;
         for(int i =1000; i<=n; i++){
-            int a =count(i);
-            count+=a;
+            count++;;
         }
       return count; }
 }
