311a228d48f7430ef4ae7589a183c8d71481f216
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-10 19:37:53 +0530

    Time: 0 ms (100%), Space: 52.8 MB (58.16%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,8 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int x =0;
-        for(int i =0;i<nums.length; i++){
-            x^=nums[i];
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if mid and its pair match
+            if (nums[mid] == nums[mid ^ 1]) {
+                low = mid + 1; // Element right half me hai
+            } else {
+                high = mid;    // Element left half me ya mid par hai
+            }
         }
-  return x;  }
+
+        return nums[low];
+    }
 }
