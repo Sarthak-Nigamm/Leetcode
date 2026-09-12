926474461cd73784e9714f8fc023d9eaaef0fb5f
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-12 11:26:15 +0530

    Time: 1 ms (56.23%), Space: 48.8 MB (14.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,21 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int[]left = new int[n];
+        int left[] = new int[n];
         left[0] = arr[0];
-        for(int i =1; i<n; i++){
-            left[i] = Math.max(left[i-1], arr[i]);
+        for(int i = 1; i<n; i++){
+            left[i] = Math.max(arr[i],left[i-1]);
         }
-
-        int [] right = new int[n];
+        int right[] = new int[n];
         right[n-1] = arr[n-1];
-        for(int i =n-2; i>=0; i--){
-            right[i] = Math.max(right[i+1],arr[i]);
+        for(int i = n-2; i>=0; i--){
+            right[i] = Math.max(right[i+1], arr[i]);
         }
 
         int twl = 0;
-        for(int i =0; i<n; i++){
-            int wl = Math.min(left[i],right[i]) - arr[i] ;
-            twl +=wl;
+        for(int i =0; i<arr.length; i++){
+            int wl = Math.min(left[i],right[i]) - arr[i];
+            twl+=wl;
         }
-  return twl;  }
-}
+
+   return twl; }}
