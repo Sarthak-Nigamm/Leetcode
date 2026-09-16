f236534fa2db2fa7ecab63026eafce3f4564b500
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-16 10:16:35 +0530

    Time: 9 ms (7.73%), Space: 48.2 MB (5.53%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int missingNumber(int[] arr) {
+            Arrays.sort(arr);
+            for(int i =0; i<arr.length; i++){
+                if(arr[i] != i){
+                    return i;
+                }
+            }        
+            return arr.length;}
+}
