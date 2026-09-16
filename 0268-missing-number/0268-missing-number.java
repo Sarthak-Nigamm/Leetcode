c6c2104ecee7dfb09f152007216e98d7e07a51de
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-16 10:20:52 +0530

    Time: 0 ms (100%), Space: 47 MB (91.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,11 +1,10 @@
 import java.util.*;
 class Solution {
     public int missingNumber(int[] arr) {
-            Arrays.sort(arr);
-            for(int i =0; i<arr.length; i++){
-                if(arr[i] != i){
-                    return i;
-                }
-            }        
-            return arr.length;}
+           int sum1 = 0, sum2 = 0;
+           for(int i =1; i<=arr.length; i++){
+            sum1 +=i;
+            sum2+=arr[i-1];
+           }
+           return sum1-sum2;}
 }
