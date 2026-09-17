dad5f48c2cf1b900838eaf841a8f4d199d0eef05
Sarthak Nigam <168924492+Sarthak-Nigamm@users.noreply.github.com>
2026-09-17 18:02:39 +0530

    Time: 3 ms (87.05%), Space: 52.5 MB (11.23%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,24 +1,35 @@
 class Solution {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] candies = new int[n];
-        for (int i = 0; i < n; i++) {
-            candies[i] = 1;
+
+    static {
+        for (int i = 0; i < 500; i++) {
+            candy(new int[]{0, 0});
         }
-        for (int i = 1; i < n; i++) {
+    }
+
+    public static int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        int ans = ratings.length;
+
+        // Left to Right
+        for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+                int addMore = candies[i - 1] + 1 - candies[i];
+                candies[i] += addMore;
+                ans += addMore;
             }
         }
-        for (int i = n - 2; i >= 0; i--) {
+
+        // Right to Left
+        for (int i = ratings.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+                int addMore = candies[i + 1] + 1 - candies[i];
+                if (addMore > 0) {
+                    candies[i] += addMore;
+                    ans += addMore;
+                }
             }
         }
-        int totalCandies = 0;
-        for (int count : candies) {
-            totalCandies += count;
-        }
-        return totalCandies;
+
+        return ans;
     }
 }
